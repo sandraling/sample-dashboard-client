7276200f4265bc4f6cb7a3f26f6cef5c634463a6
Sandra Ling <s.sandra.ling@gmail.com>
2021-06-03 23:28:31 -0400

    Hotfix: Modified capitalization of an imported file in attempt to fix build error in heroku

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { App } from "./app";
+import { App } from "./App";
 import './style.scss';
 import "highcharts/css/highcharts.css";
 
